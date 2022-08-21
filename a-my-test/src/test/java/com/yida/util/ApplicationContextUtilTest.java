93d7b0351c40a2644d7fa7137c5a530fa153ee2c
happy-to-code <yida8080@gmail.comc>
2022-08-21 19:34:38 +0800

    juit

--- a-my-test/src/test/java/com/yida/util/ApplicationContextUtilTest.java
+++ a-my-test/src/test/java/com/yida/util/ApplicationContextUtilTest.java
@@ -2,10 +2,9 @@ package com.yida.util;
 
 import com.yida.pojo.Student;
 import com.yida.service.StudentService;
-// import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
