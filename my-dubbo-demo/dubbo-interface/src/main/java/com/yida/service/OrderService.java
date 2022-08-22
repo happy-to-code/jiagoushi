7d5641364682b0215e0db39a86f34200421b18ab
happy-to-code <yida8080@gmail.comc>
2022-08-23 00:36:37 +0800

    dubbo demo（todo）

--- my-dubbo-demo/dubbo-interface/src/main/java/com/yida/service/OrderService.java
+++ my-dubbo-demo/dubbo-interface/src/main/java/com/yida/service/OrderService.java
@@ -1,11 +1,12 @@
 package com.yida.service;
 
 public interface OrderService {
-
-    /**
-     * 获取订单详情
-     * @param orderId
-     * @return
-     */
-    String getOrder(Long orderId);
+	
+	/**
+	 * 获取订单详情
+	 *
+	 * @param orderId
+	 * @return
+	 */
+	String getOrder(Long orderId);
 }
